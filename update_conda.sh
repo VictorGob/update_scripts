cf9665bd6b4580357cde95a3910843f4a735cef1
Víctor <VictorGob@users.noreply.github.com>
2023-11-08 10:27:38 +0100

    Update update_conda.sh
    Check if "base" enviroment is active

--- update_conda.sh
+++ update_conda.sh
@@ -1,3 +1,12 @@
-#/bin/bash
+#!/bin/bash
+
+# Detect CONDA_DEFAULT_ENV if set and it's "base"
+if [[ -z "${CONDA_DEFAULT_ENV}" ]] || [[ "${CONDA_DEFAULT_ENV}" != "base" ]]; then
+    echo ""
+    echo "!!! Conda enviroment not set to 'base'. Exiting."
+    echo ""
+    exit 1
+fi
+
 conda update --all -y
 conda clean --all -y
